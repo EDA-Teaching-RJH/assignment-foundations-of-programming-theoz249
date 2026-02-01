adb1d835ec7b72d216fd403d8e49b23d9b389d82
theoz249 <tz66@kent.ac.uk>
2026-02-01 16:47:17 +0000

    i

--- old_system.py
+++ old_system.py
@@ -42,13 +42,17 @@ def run_system_monolith():
             print("Crew member added.")
             
         elif opt == "3":
-            rem = input("Name to remove: ").title()
+            if len(n) != 0 :
+                rem = input("Name to remove: ").title()
 
-            idx = n.index(rem)
-            n.pop(idx)
-            r.pop(idx)
-            d.pop(idx)
-            print("Removed.")
+                idx = n.index(rem)
+                n.pop(idx)
+                r.pop(idx)
+                d.pop(idx)
+                print("Removed.")
+            else:
+                print("Invalid.")
+                
             
         elif opt == "4":
             print("Analyzing...")
