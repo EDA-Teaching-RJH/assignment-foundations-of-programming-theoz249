1f4df7c7c0bcfafc310f540c3b67ec8795f144e9
theoz249 <tz66@kent.ac.uk>
2026-02-01 19:41:07 +0000

    part 1

---
+++ fleet_manager.py
@@ -0,0 +1,37 @@
+import time
+name = ["Picard", "Riker", "Data", "Worf", "Spock"]
+rank = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Commader"]
+div = ["Command", "Command", "Operations", "Security", "Science"]
+id = ["0000", "1111", "2222", "3333", "4444"]
+
+loading = 0
+while loading < 5 :
+    print("System loading" , loading)
+    loading += 1
+    time.sleep(0.1)
+    if loading  == 5 :
+        print("Systems online")
+
+def main():
+    
+
+def init_database():
+
+def display_menu():
+
+def add_member():
+
+def remove_member():
+
+def update_rank():
+
+def display_roster():
+
+def search_crew():
+
+def filter_by_division():
+
+def calculate_payroll():
+
+def count_officers():
+
